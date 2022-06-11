52d4825083a5b3797334006f3b96a58137cc5d75
Zokirjs <Zokirjs@yandex.com>
2022-06-12 03:57:19 +0500

    login page 10%

--- public/src/App.js
+++ public/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import Chat from './pages/Chat';
+import './App.css';
 
 export default function App () {
   return <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
+      <Route path="/" element={<Chat />} />
     </Routes>
   </BrowserRouter>;
 }
--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import style from "styled-components";
+import logo from "../assets/logo.png";
 
 function Login() {
-    return <div>Login</div>;
+    return <LoginDiv>
+            <LoginContainer>
+                <LoginLogo src={logo} alt="logo" />
+            </LoginContainer>
+        </LoginDiv >
 }
+
+const LoginDiv = style.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #3498db;
+    height: 100vh;
+    margin: auto;
+    align-items: center;
+    justify-content: center;
+`;
+const LoginContainer = style.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #f3f3f3;
+    border-radius: 5px;
+    box-shadow: 0px 0px 60px #f3f3f3;
+    width: 500px;
+    height: 300px;
+`;
+const LoginLogo = style.img`
+    width: 100px;
+    height: 100px;  
+`
+
 
 export default Login;
